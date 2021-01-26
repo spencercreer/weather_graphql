647455cc91ea943049beabc85363efc9642d57c6
spencercreer <spencercreer@gmail.com>
2021-01-25 21:49:42 -0700

    changed formatting

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
     var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let dt = new Date();
-    
+
+        
+    $(".currentDay").text(weekdays[dt.getDay()]);
+    $(".currentDate").text(`${dt.getMonth() + 1} / ${dt.getDate()} / ${dt.getFullYear()}`);
     $(".wday1").text(weekdays[dt.getDay()+1]);
     $(".day1").text(`${dt.getMonth() + 1} / ${dt.getDate() + 1} / ${dt.getFullYear()}`);
     $(".wday2").text(weekdays[dt.getDay()+2]);
@@ -13,6 +16,8 @@ $(document).ready(function() {
     $(".wday5").text(weekdays[dt.getDay()+5]);
     $(".day5").text(`${dt.getMonth() + 1} / ${dt.getDate() + 5} / ${dt.getFullYear()}`);
 
+    let cityEl = $("<a>");
+    let storedCity = "";
 
     function convertTemp(tempK){
         //Convert temp Kelvin to Fahrenheit
@@ -31,11 +36,7 @@ $(document).ready(function() {
           method: "GET"
         }).then(function(response) {
     
-            // Log the queryURL
-            console.log(queryURL);
-    
-            // Log the resulting object
-            console.log(response);
+            // Get latitude and longitude of searched city
             var latitude = response.coord.lat;
             var longitude = response.coord.lon;
     
@@ -46,7 +47,7 @@ $(document).ready(function() {
                url: queryURL2,
                method: "GET"
            }).then(function(response2) {
-               console.log(response2);
+               
                // Transfer current weather content to current forecast HTML
                $(".city").text(city.charAt(0).toUpperCase()+city.slice(1));
                $(".icon").attr("src","http://openweathermap.org/img/wn/"+response2.current.weather[0].icon+"@2x.png")
@@ -55,15 +56,19 @@ $(document).ready(function() {
                $(".humidity").text("Humidity: " + response2.current.humidity + "%");
                $(".wind").text("Wind Speed: " + response2.current.wind_speed + " m/s");
                let uvi = response2.current.uvi;
+
                if(uvi<=2){
-                $(".uvIndex").attr("class","uvIndex rounded px-2 bg-success");
-                $(".uvIndex").text("UV Index: " + uvi + " Favorable");
+                   // UV index less than equal to 2 favorable, green
+                   $(".uvIndex").attr("class","uvIndex rounded px-2 bg-success");
+                   $(".uvIndex").text("UV Index: " + uvi + " Favorable");
                } else if(uvi>2 && uvi<=7){
-                $(".uvIndex").attr("class","uvIndex rounded px-2 bg-warning");
-                $(".uvIndex").text("UV Index: " + uvi + " Moderate");
+                   // UV index less between 2 and 7 moderate, yellow
+                   $(".uvIndex").attr("class","uvIndex rounded px-2 bg-warning");
+                   $(".uvIndex").text("UV Index: " + uvi + " Moderate");
                } else{
-                $(".uvIndex").attr("class","uvIndex rounded px-2 bg-danger");
-                $(".uvIndex").text("UV Index: " + uvi + " Severe");
+                   // UV index greater than 7 severe, red
+                   $(".uvIndex").attr("class","uvIndex rounded px-2 bg-danger");
+                   $(".uvIndex").text("UV Index: " + uvi + " Severe");
                }
     
                // Transfer daily content to five day forecast HTML
@@ -80,18 +85,40 @@ $(document).ready(function() {
     }
 
     $(".searchBtn").click(function(){
+        // Search button click, get search input value and call API
         cityInput = $(".city-input").val();
         callAPI(cityInput);
-        cityEl = $("<a>");
+        // Add searched city to cities list
+        let cityEl = $("<a>");
         cityEl.text(cityInput.charAt(0).toUpperCase()+cityInput.slice(1));
         cityEl.attr("class","list-group-item list-group-item-action");
         $(".cityList").prepend(cityEl);
+        // Add searched city to localStorage
+        localStorage.setItem("searchedCity",cityInput.charAt(0).toUpperCase()+cityInput.slice(1));
     })
 
     $(document.body).on("click", "a", function(){
+        // City list element click, get list element city and call API
         cityClick = $(this).text();
         callAPI(cityClick);
+        // Add clicked city to localStorage element
+        localStorage.setItem("searchedCity",cityClick);
     })
 
-    callAPI("New York");
+    if(!localStorage.getItem("searchedCity")){
+        localStorage.setItem("searchedCity","New York");
+        storedCity = localStorage.getItem("searchedCity");
+        callAPI(storedCity);
+        // Add New York to cities list
+        cityEl.text(storedCity);
+        cityEl.attr("class","list-group-item list-group-item-action");
+        $(".cityList").prepend(cityEl);
+    } else{
+        storedCity = localStorage.getItem("searchedCity");
+        callAPI(storedCity);
+        // Add stored city to cities list
+        cityEl.text(storedCity);
+        cityEl.attr("class","list-group-item list-group-item-action");
+        $(".cityList").prepend(cityEl);
+    }
 })
